f4ac8d3d72a7d2c9ee743c645a13aea4ae1219fa
Neil Graneros Flores <neilgraneros11@gmail.com>
2026-04-01 19:00:12 -0400

    feat: :sparkles: implementar servicio y ruta para el inicio de sesión

---
+++ app/api/login/login.service.ts
@@ -0,0 +1,45 @@
+import { LoginData, User } from "@/types";
+import { SignupRepository } from "../signup/signup.repository";
+import bcrypt from "bcryptjs";
+import { signToken } from "@/lib/auth";
+
+export class LoginService {
+  constructor(private signupRepository = new SignupRepository()) {}
+
+  async login(data: LoginData): Promise<{ token: string } & User> {
+    try {
+      const { email, password } = data;
+
+      if (!email || !password) {
+        throw new Error("All fields are required");
+      }
+
+      if (password.length < 6)
+        throw new Error("Password must be at least 6 characters long");
+
+      const existingUser = await this.signupRepository.findUserByEmail(email);
+      if (!existingUser) {
+        throw new Error("Email already registered");
+      }
+
+      const isValid = await bcrypt.compare(password, existingUser.password);
+
+      if (!isValid) {
+        throw new Error("Invalid email or password");
+      }
+
+      const token = signToken({
+        userId: existingUser.id,
+        email: existingUser.email,
+      });
+
+      return { token, ...existingUser };
+    } catch (error) {
+      throw new Error(
+        error instanceof Error
+          ? error.message
+          : "Error occurred while signing up",
+      );
+    }
+  }
+}
---
+++ app/api/login/route.ts
@@ -0,0 +1,29 @@
+import { LoginData } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+import { LoginService } from "./login.service";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body: LoginData = await req.json();
+    const resultado = await new LoginService().login(body);
+    const response = NextResponse.json(
+      { success: true, token: resultado.token },
+      { status: 200 },
+    );
+
+    response.cookies.set("token", resultado.token, {
+      httpOnly: true,
+      path: "/",
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: error instanceof Error ? error.message : "Error al logearse",
+      },
+      { status: 400 },
+    );
+  }
+}
