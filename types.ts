f0f6e60ccaaaafa887b386cd2c31fb23b0c8758b
Neil Graneros Flores <neilgraneros11@gmail.com>
2026-04-01 19:33:25 -0400

    feat: :sparkles: agregar interfaces para los reviews

--- types.ts
+++ types.ts
@@ -20,3 +20,21 @@ export interface LoginData {
   email: string;
   password: string;
 }
+
+export interface Review {
+  id: number;
+  user_id: number;
+  book_title: string;
+  rating: number;
+  review: string;
+  mood: string;
+  created_at: Date | string;
+}
+
+export interface CreateReviewData {
+  user_id: number;
+  book_title: string;
+  rating: number;
+  review: string;
+  mood: string;
+}
