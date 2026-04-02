b24ae6bdcc22434826dad83c792be13d2ebe07f6
Neil Graneros Flores <neilgraneros11@gmail.com>
2026-04-02 11:28:44 -0400

    feat: :sparkles: agregar la página Login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/src/components/LoginForm/LoginForm";
+
+const Login = () => {
+  return (
+    <div>
+      Login
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
